5394ecc1fa701dde236bafc7309a278aaa12715e
garrisonglass <garrison.glass@gmail.com>
2025-09-09 21:35:32 -0400

    Add project files.

---
+++ IDE and VCS Workflow Project.cpp
@@ -0,0 +1,16 @@
+// IDE and VCS Workflow Project.cpp 
+// This program displays your initials.
+
+#include <iostream>
+using namespace std;
+int main(){
+    char first, middle, last;
+// Store your initial in these variables
+    first = 'G';
+    middle = 'H';
+    last = 'G';
+    cout << first << middle << last << endl;
+    return 0;
+    
+}
+
